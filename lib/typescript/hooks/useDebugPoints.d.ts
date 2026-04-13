468bd2df60ae442f6117a756810d3a83fa1c8421
thomasvo <thomas.vo@openspace.ai>
2026-04-13 15:31:06 -0700

    Strip lib/ build artifacts made redundant by #158
    PR #158 removed checked-in lib/ and added it to .gitignore.
    These remaining tracked files are now redundant.

--- lib/typescript/hooks/useDebugPoints.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { GestureTouchEvent } from 'react-native-gesture-handler';
-import { Vec2D } from '../typings';
-export declare const useDebugPoints: () => {
-    debugPoints: Vec2D[];
-    setDebugPoints: import("react").Dispatch<import("react").SetStateAction<Vec2D[]>>;
-    setPinchDebugPoints: (e: GestureTouchEvent, zoomCenter: Vec2D, ...points: Vec2D[]) => void;
-};
-//# sourceMappingURL=useDebugPoints.d.ts.map
