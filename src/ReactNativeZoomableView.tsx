0a6365b927f4d855ae6aa6e8fa7a45ed1be0991b
thomasvo <thomas.vo@openspace.ai>
2026-05-03 18:03:10 -0700

    Revert "Mark worklet-eligible functions as worklets"
    This reverts commit 6edf1e549a0c8f10de7f1da29b2b3b90617cb1fd.

--- src/ReactNativeZoomableView.tsx
+++ src/ReactNativeZoomableView.tsx
@@ -553,7 +553,7 @@ const ReactNativeZoomableViewInner: ForwardRefRenderFunction<
 
       // == Perform Zoom Animation ==
       zoom.value = withTiming(newZoomLevel, zoomToAnimation, () => {
-        'worklet';
+        'worlet';
 
         // == Zoom Animation Ends ==
         zoomToDestination.value = undefined;
